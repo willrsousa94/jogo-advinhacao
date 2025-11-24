c008032c6c08f112cde6c8223ac7efc0ee3aeb08
William Sousa <williamrsousa94@hotmail.com>
2025-11-24 13:31:03 -0300

    adicionado mensagem de aviso caso o número digitado não seja entre 1 e 100

--- script.js
+++ script.js
@@ -9,31 +9,40 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   //Criação do evento direto na hora do clique no botão de Chutar.
   document.querySelector("#chutar").addEventListener("click", function (e) {
-    if (tentativas > 0) {
-      if (
-        Number(document.querySelector("#palpite").value) < numero ||
-        Number(document.querySelector("#palpite").value) > numero
-      ) {
-        console.log(Number(document.querySelector("#palpite").value));
-        tentativas--;
-        if (tentativas <= 5) {
-          document.querySelector("#resultado").innerText =
-            Number(document.querySelector("#palpite").value) < numero
-              ? `Errou, bobinho, tente um número maior`
-              : `Errou, bobinho, tente um número menor`;
-        }
-        if (tentativas < 1) {
+    if (
+      Number(
+        document.querySelector("#palpite").value < 1 ||
+          Number(document.querySelector("#palpite").value > 100)
+      )
+    ) {
+      alert("Por favor, digite um número entre 1 e 100");
+    } else {
+      if (tentativas > 0) {
+        if (
+          Number(document.querySelector("#palpite").value) < numero ||
+          Number(document.querySelector("#palpite").value) > numero
+        ) {
+          console.log(Number(document.querySelector("#palpite").value));
+          tentativas--;
+          if (tentativas <= 5) {
+            document.querySelector("#resultado").innerText =
+              Number(document.querySelector("#palpite").value) < numero
+                ? `Errou, bobinho, tente um número maior`
+                : `Errou, bobinho, tente um número menor`;
+          }
+          if (tentativas < 1) {
+            document.querySelector(
+              "#resultado"
+            ).innerText = `Que pena meu consagrado, você não me venceu, o número é ${numero}, não era óbvio?`;
+          }
+          document.querySelector("#tentativa-restante").innerText = tentativas;
+        } else {
           document.querySelector(
             "#resultado"
-          ).innerText = `Que pena meu consagrado, você não me venceu, o número é ${numero}, não era óbvio?`;
+          ).innerText = `Parabéns, você acertou, o número era ${Number(
+            document.querySelector("#palpite").value
+          )}`;
         }
-        document.querySelector("#tentativa-restante").innerText = tentativas;
-      } else {
-        document.querySelector(
-          "#resultado"
-        ).innerText = `Parabéns, você acertou, o número era ${Number(
-          document.querySelector("#palpite").value
-        )}`;
       }
     }
   });
